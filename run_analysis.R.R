f5f0418b35b0e27f1c9df8db9f13193ec03079e2
dliulhd <dliulhd@gmail.com>
2015-06-19 12:51:37 -0400

    release memory lines added

--- run_analysis.R.R
+++ run_analysis.R.R
@@ -14,13 +14,13 @@ for(k in 1:length(grp_mean)) {grpMSMS[2*k-1] <- grp_mean[k]; grpMSMS[2*k] <- grp
 name_vec <- c("Subject","Action",as.character(features$V2[grpMSMS]))
 
 
-# following six lines work on the test data set
+## following six lines work on the test data set
 Xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
 Ytest <- read.table("UCI HAR Dataset/test/y_test.txt")
 Subj_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 Select_Test <- cbind(Subj_test$V1,activities$V2[Ytest$V1],Xtest[,grpMSMS])
 names(Select_Test) <- name_vec
-#rm(Xtest)
+rm(Xtest) #release memory
 
 # following six lines work on the training data set
 Xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
@@ -28,10 +28,11 @@ Ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
 Subj_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 Select_Train <- cbind(Subj_train$V1,activities$V2[Ytrain$V1],Xtrain[,grpMSMS])
 names(Select_Train) <- name_vec
-#rm(Xtrain)
+rm(Xtrain) #release memory
 
-# row-binding together the testing and the training data frames and then export to txt
+## row-binding together the testing and the training data frames and then export to txt
 Tidy_Data <- rbind(Select_Test,Select_Train)
+rm(Select_Test,Select_Train) #release memory
 write.table(Tidy_Data,file="Tidy_Data.txt",row.names = FALSE) 
 
 
